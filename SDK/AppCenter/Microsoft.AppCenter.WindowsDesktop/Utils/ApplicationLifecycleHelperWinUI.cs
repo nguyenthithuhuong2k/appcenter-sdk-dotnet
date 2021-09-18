727294370b9e72f58ca0627ae83add0b36985da6
Anastasia Kubova <v-ankubo@microsoft.com>
2021-09-18 08:54:35 +0300

    Fix sending startSession for WinUI apps

--- SDK/AppCenter/Microsoft.AppCenter.WindowsDesktop/Utils/ApplicationLifecycleHelperWinUI.cs
+++ SDK/AppCenter/Microsoft.AppCenter.WindowsDesktop/Utils/ApplicationLifecycleHelperWinUI.cs
@@ -10,6 +10,8 @@ using System.Threading.Tasks;
 using Windows.ApplicationModel.Core;
 using Windows.Foundation.Metadata;
 using Windows.UI.Core;
+using Microsoft.UI.Xaml;
+using Windows.UI.ViewManagement.Core;
 
 namespace Microsoft.AppCenter.Utils
 {
@@ -20,7 +22,7 @@ namespace Microsoft.AppCenter.Utils
         public event EventHandler<UnhandledExceptionOccurredEventArgs> UnhandledExceptionOccurred;
 
         // True if InvokeResuming has been called at least once during the current process
-        private static bool _started;
+        private static bool _isAppStarted;
         
         // Considered to be suspended until can verify that has started
         private static bool _suspended = true;
@@ -42,35 +44,10 @@ namespace Microsoft.AppCenter.Utils
 
         public ApplicationLifecycleHelper()
         {
+
             // Subscribe to Resuming and Suspending events.
             CoreApplication.Suspending += InvokeSuspended;
-
-            // If the "LeavingBackground" event is present, use that for Resuming. Else, use CoreApplication.Resuming.
-            if (ApiInformation.IsEventPresent(typeof(CoreApplication).FullName, "LeavingBackground"))
-            {
-                CoreApplication.LeavingBackground += InvokeResuming;
-
-                // If the application has anything visible, then it has already started,
-                // so invoke the resuming event immediately.
-                HasStartedAndNeedsResume().ContinueWith(completedTask =>
-                {
-                    if (completedTask.Result)
-                    {
-                        InvokeResuming(null, EventArgs.Empty);
-                    }
-                });
-            }
-            else
-            {
-                // In versions of Windows 10 where the LeavingBackground event is unavailable, we consider this point to be
-                // the start so invoke resuming (and subscribe to future resume events). If InvokeResuming were not called here,
-                // the resuming event wouldn't be invoked until the *next* time the application is resumed, which is a problem
-                // if the application is not currently suspended. The side effect is that regardless of whether UI is available
-                // ever in the process, InvokeResuming will be called at least once (in the case where LeavingBackground isn't
-                // available).
-                CoreApplication.Resuming += InvokeResuming;
-                InvokeResuming(null, EventArgs.Empty);
-            }
+            CoreInputView.GetForCurrentView().OcclusionsChanged += V_OcclusionsChanged;
 
             // Subscribe to unhandled errors events.
             CoreApplication.UnhandledErrorDetected += (sender, eventArgs) =>
@@ -91,41 +68,16 @@ namespace Microsoft.AppCenter.Utils
             };
         }
 
-        // Determines whether the application has started already and is not suspended, 
-        // but ApplicationLifecycleHelper has not yet fired an initial "resume" event.
-        private static async Task<bool> HasStartedAndNeedsResume()
+        private void V_OcclusionsChanged(CoreInputView sender, CoreInputViewOcclusionsChangedEventArgs args)
         {
-            var needsResume = false;
-            try
+            if (_isAppStarted)
             {
-                // Don't use CurrentSynchronizationContext as that seems to cause an error in Unity applications.
-                var asyncAction = CoreApplication.MainView?.CoreWindow?.Dispatcher.RunAsync(
-                    CoreDispatcherPriority.Normal, () =>
-                    {
-                        // If started already, a resume has already occurred.
-                        if (_started)
-                        {
-                            return;
-                        }
-                        if (CoreApplication.Views.Any(view => view.CoreWindow != null &&
-                                                              view.CoreWindow.Visible))
-                        {
-                            needsResume = true;
-                        }
-                    });
-                if (asyncAction != null)
-                {
-                    await asyncAction;
-                }
-            }
-            catch (Exception e) when (e is COMException || e is InvalidOperationException)
+                InvokeSuspended(null, EventArgs.Empty);
+            } 
+            else
             {
-                // If MainView can't be accessed, a COMException or InvalidOperationException is thrown. It means that the
-                // MainView hasn't been created, and thus the UI hasn't appeared yet.
-                AppCenterLog.Debug(AppCenterLog.LogTag,
-                    "Not invoking resume immediately because UI is not ready.");
+                InvokeResuming(null, EventArgs.Empty);
             }
-            return needsResume;
         }
 
         internal void InvokeUnhandledExceptionOccurred(object sender, Exception exception)
@@ -135,7 +87,7 @@ namespace Microsoft.AppCenter.Utils
 
         private void InvokeResuming(object sender, object e)
         {
-            _started = true;
+            _isAppStarted = true;
             _suspended = false;
             ApplicationResuming?.Invoke(sender, EventArgs.Empty);
         }
@@ -143,6 +95,7 @@ namespace Microsoft.AppCenter.Utils
         private void InvokeSuspended(object sender, object e)
         {
             _suspended = true;
+            _isAppStarted = false;
             ApplicationSuspended?.Invoke(sender, EventArgs.Empty);
         }
     }
